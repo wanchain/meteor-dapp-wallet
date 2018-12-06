ecfb80b537866267791694c36c8ffd234697c471
Sean chai <chaiwl123@163.com>
2018-12-06 16:01:35 +0800

    show approveZeroTxHash

--- app/client/templates/elements/crossErc20History.js
+++ app/client/templates/elements/crossErc20History.js
@@ -603,6 +603,7 @@ Template['elements_cross_transactions_table_erc20'].events({
                     crossAddress: show_data.crossAddress,
                     from: show_data.from,
                     approveTxHash: show_data.approveTxHash,
+                    approveZeroTxHash: show_data.approveZeroTxHash,
                     lockTxHash: show_data.lockTxHash,
                     redeemTxHash: show_data.redeemTxHash,
                     revokeTxHash: show_data.revokeTxHash,
